75a905aa615c8c89d451d63f80b5cd06c8fabefd
Feng <jumphone@outlook.com>
2026-03-27 11:44:13 +0800

    update

--- supp/environment.sh
+++ supp/environment.sh
@@ -1,9 +1,11 @@
-PYTHON_TGZ_PATH=/home/toolkit/tools/Python3.12.13/
+PYTHON_TGZ_PATH=/home/toolkit/tools/Python-3.12.13/
 PYTHON_BIN_PATH=/home/toolkit/local_py312/bin/
+
+OPENSSL=/opt/openssl
 
 :<<!
 ${PYTHON_TGZ_PATH}configure --prefix=/home/toolkit/local_py312/ \
-            --with-openssl=/opt/openssl \
+            --with-openssl=${OPENSSL} \
             --with-openssl-rpath=auto
 ${PYTHON_BIN_PATH}pip3 download -r docs/requirements.txt -d /home/toolkit/tools/ai_local_src/pip_packages
 ${PYTHON_BIN_PATH}pip3 install -r docs/requirements.txt --no-index --find-links=/home/toolkit/tools/ai_local_src/pip_packages 
