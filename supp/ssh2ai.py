e5f2e46f7de5330542122da254914965daad28b3
Feng <jumphone@outlook.com>
2026-03-30 18:24:14 +0800

    update

--- supp/ssh2ai.py
+++ supp/ssh2ai.py
@@ -3,9 +3,10 @@ import os,time
 import subprocess as sp
 from datetime import datetime
 
-file_path='/home/media/my_voice_tmp.txt'
-print('-'*30) 
+DASH_LINE='-'*50
 
+file_dir='/home/media/voice_dir/'
+print(DASH_LINE) 
 
 def get_timestamp():
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -17,21 +18,26 @@ def print_ts(text):
     print(output)
 
 while True:
-    if os.path.exists(file_path):
-        try:
-            print_ts('Message:')
-            print()
-            file_content=open(file_path,'r').read()
-            print(file_content)
-            os.remove(file_path)   
-            print('-'*30)
-            print_ts('Result:') 
-            print()
-            sp.Popen(['/bin/ai', file_content]).wait()
-            print()
-            print('-'*30) 
-        except Exception as e:
-            print("ERROR:", e)
+    file_list= os.listdir(file_dir)
+    if len(file_list)>0:
+        for file_name in file_list:
+            print(file_name)
+            print(DASH_LINE)
+            file_path=os.path.join(file_dir, file_name)
+            try:
+                print_ts('Message:')
+                print()
+                file_content=open(file_path,'r').read()
+                print(file_content)
+                os.remove(file_path)   
+                print(DASH_LINE)
+                print_ts('Result:') 
+                print()
+                sp.Popen(['/home/media/ai/bin/aiw.py', file_content]).wait()
+                print()
+                print(DASH_LINE) 
+            except Exception as e:
+                print("ERROR:", e)
     time.sleep(1)    
 
 
